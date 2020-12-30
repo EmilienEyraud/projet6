69c8388c6d4e41c0bffedcfabf091b5325f83adb
Emilieneyraud <direction@groupe-socom.fr>
2020-12-30 18:30:49 +0100

    default_likes_dislikes

--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -8,8 +8,8 @@ const sauceSchema = mongoose.Schema({
     mainPepper: { type: String, required: true }, // Principal ingrédient de la sauce
     imageUrl: { type: String, required: true }, //String de l'image téléchargé pare l'utilisateur
     heat: { type: Number, required: true }, // Nombre entre 1 et 10 décrivant la sauce
-    likes: { type: Number, required: false },//Nombre d'utilisateurs qui aiment la sauce
-    dislikes: { type: Number, required: false },//Nombre d'utilisateurs qui n'aiment pas la sauce
+    likes: { type: Number, required: false, default : 0 },//Nombre d'utilisateurs qui aiment la sauce
+    dislikes: { type: Number, required: false, default: 0 },//Nombre d'utilisateurs qui n'aiment pas la sauce
     usersLiked: { type: [String], required: false }, //Tableau d'identifiants des utilisateurs qui aiment la sauce
     usersDisliked: { type: [String], required: false },//Tableau d'identifiants des utilisateurs qui n'aiment pas la sauce
 });
